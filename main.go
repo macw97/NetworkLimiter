4b15165af5119b49fc6682142384d40c7c3077f3
macw97 <maciek.wojtowicz97@gmail.com>
2024-04-21 11:27:33 -0400

    Accepts set of ips to blacklist, parameter validity checks

--- main.go
+++ main.go
@@ -4,12 +4,14 @@ import (
 	"flag"
 	"fmt"
 	"net"
+	"regexp"
+	"strings"
 
 	"github.com/google/nftables"
 	"github.com/google/nftables/expr"
 )
 
-func createFilter(ip string, bandwidth int, packetsPerS int) {
+func createFilter(ips []string, bandwidth int, packetsPerS int) {
 	connections := &nftables.Conn{}
 
 	if kernel_tabs, err := connections.ListTables(); err == nil {
@@ -37,6 +39,25 @@ func createFilter(ip string, bandwidth int, packetsPerS int) {
 		Priority: nftables.ChainPriorityFilter,
 	})
 
+	set := &nftables.Set{
+		Name:    "blacklist",
+		Table:   table,
+		KeyType: nftables.TypeIPAddr,
+	}
+
+	var setData []nftables.SetElement
+	for _, ip := range ips {
+		elem := nftables.SetElement{
+			Key: net.ParseIP(ip).To4(),
+		}
+
+		setData = append(setData, elem)
+	}
+
+	if err := connections.AddSet(set, setData); err != nil {
+		fmt.Println("IP addr set error - ", err)
+	}
+
 	if packetsPerS != 0 {
 		connections.AddRule(&nftables.Rule{
 			Table: table,
@@ -49,10 +70,10 @@ func createFilter(ip string, bandwidth int, packetsPerS int) {
 					Len:          4,
 				},
 
-				&expr.Cmp{
-					Op:       expr.CmpOpEq,
-					Register: 1,
-					Data:     net.ParseIP(ip).To4(),
+				&expr.Lookup{
+					SourceRegister: 1,
+					SetName:        set.Name,
+					SetID:          set.ID,
 				},
 
 				&expr.Limit{
@@ -81,18 +102,17 @@ func createFilter(ip string, bandwidth int, packetsPerS int) {
 					Len:          4,
 				},
 
-				&expr.Cmp{
-					Op:       expr.CmpOpEq,
-					Register: 1,
-					Data:     net.ParseIP(ip).To4(),
+				&expr.Lookup{
+					SourceRegister: 1,
+					SetName:        set.Name,
+					SetID:          set.ID,
 				},
 
 				&expr.Limit{
-					Type:  expr.LimitTypePktBytes,
-					Rate:  uint64(bandwidth),
-					Over:  true,
-					Unit:  expr.LimitTimeSecond,
-					Burst: 1,
+					Type: expr.LimitTypePktBytes,
+					Rate: uint64(bandwidth),
+					Over: true,
+					Unit: expr.LimitTimeSecond,
 				},
 
 				&expr.Verdict{
@@ -124,7 +144,7 @@ func main() {
 	var packetsPerS int
 
 	flag.BoolVar(&help, "h", false, "Command line arguments help")
-	flag.StringVar(&ip, "ip", "127.0.0.1", "Ip address with which limit traffic")
+	flag.StringVar(&ip, "ip", "127.0.0.1,10.10.10.10", "Ip address with which limit traffic or set of ips")
 	flag.IntVar(&bandwidth, "b", 0, "Bandwidth limit in kpbs")
 	flag.IntVar(&packetsPerS, "pkt", 0, "Packets per second")
 	flag.Parse()
@@ -134,7 +154,14 @@ func main() {
 		fmt.Println("ip:", ip, "\nbandwidth (kbps):", bandwidth, "\nPackets per second:", packetsPerS)
 	}
 
-	// Make parameters valid check
+	if !regexp.MustCompile("([0-9]{1,3}\\.){3}[0-9]{1,3}").MatchString(ip) {
+		fmt.Println("Incorrect ip address")
+	} else {
 
-	createFilter(ip, bandwidth, packetsPerS)
+		ip_table := strings.Split(ip, ",")
+		fmt.Println("ips - ", ip_table)
+		if bandwidth != 0 || packetsPerS != 0 {
+			createFilter(ip_table, bandwidth, packetsPerS)
+		}
+	}
 }
